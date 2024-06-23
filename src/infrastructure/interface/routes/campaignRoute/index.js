687fa39d7eaf7c798f956740e4429d40e26874c3
Mac <mac@Macs-MacBook-Pro.local>
2024-06-23 18:11:50 +0700

    [EDIT] Route root

--- src/infrastructure/interface/routes/campaignRoute/index.js
+++ src/infrastructure/interface/routes/campaignRoute/index.js
@@ -1,9 +1,9 @@
 const { Router, Request, Response } = require("express");
-const { createCampaign } = require("../../../app/use_cases/campaign_use_case");
+const { createCampaign } = require("../../../../app/use_cases/campaign_use_case");
 const {
   campaignValidate,
-} = require("../../../app/middlewares/validators/campaign.validator");
-const { responseCode } = require('../../../app/middlewares/services/responseCode');
+} = require("../../../../app/middlewares/validators/campaign.validator");
+const { responseCode } = require('../../../../app/middlewares/services/responseCode');
 
 const router = Router();
 
--- src/infrastructure/interface/routes/index.js
+++ src/infrastructure/interface/routes/index.js
@@ -1,9 +1,9 @@
-const Routes = require('./route');
+const campaignRoutes = require('./campaignRoute');
 
 module.exports = (app) => {
   console.log('get this route');
   app.get("/", (req, res) => {
     res.json({ message: "API is connected! LOL" });
   });
-  app.use(Routes);
+  app.use(campaignRoutes);
 };
